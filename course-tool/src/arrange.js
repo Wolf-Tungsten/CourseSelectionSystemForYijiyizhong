46f50b82b5e5e43edb8c8c721544b2b124d9c0cb
Wolf Tungsten <gaoruihao@outlook.com>
2017-08-15 17:30:17 +0800

    算法订正

--- course-tool/src/arrange.js
+++ course-tool/src/arrange.js
@@ -10,9 +10,12 @@ let course = JSON.parse(fs.readFileSync(path.join(__dirname, '../final2.course')
 let gradeList=new Object();
 let nameList=new Object();
 let lowControl=new Object();
+let successCounter = 0;
+let Counter = 0;
+
 //从工作薄加载成绩
 log('[+]正在加载成绩单');
-let workbook = xlsx.readFile(path.join(__dirname, '../高一期中数据2.xlsx'));
+let workbook = xlsx.readFile(path.join(__dirname, '../高一期中数据删除.xlsx'));
 for(let subjectIndex in workbook.SheetNames){
     let sheetName = workbook.SheetNames[subjectIndex];
     let sheet = workbook.Sheets[sheetName];
@@ -93,76 +96,73 @@ for(let subject in course){
     lowControl[subject] = lowGrade;
 }
 log(course);
+
+/*
 for (let number in nameList)
 {
+    Counter++;
     log('[+] '+nameList[number].name+'开始排课');
-    for (let attemp=0; attemp<20;attemp++){//每个学生尝试10次
+    for (let attemp=0; attemp<=20;attemp++){//每个学生尝试10次
         let successCount=0;//成功排课的数量
         let success = false;
+        nameList[number].course.sort((a,b)=>{
+            return getCourse(a.subject,a.grade,nameList[number].time).length - getCourse(b.subject,b.grade,nameList[number].time).length;
         for(let subjectIndex in nameList[number].course)
         {
-            nameList[number].course.sort((a,b)=>{
-                return getCourse(a.subject,a.grade,nameList[number].time).length - getCourse(b.subject,b.grade,nameList[number].time).length;
-            });
             let subject = nameList[number].course[subjectIndex].subject;
-            let rank = '';
-            let available = getCourse(subject,
-                nameList[number].course[subjectIndex].grade,
-                nameList[number].time
-            );
-            if(available.length>0){
-                let select = available[Math.floor(Math.random()*available.length)]; //随机选出一门可用的课程
-                rank = select.rank;
-                nameList[number].time.push(select.course.time);//增加个人时间占用
-                nameList[number].select.push({
-                    name:select.course.name,
-                    rank:rank,
-                    subject:subject}
+            if(nameList[number].course[subjectIndex].grade>=lowControl[subject]) {
+                let rank = '';
+                let available = getCourse(subject,
+                    nameList[number].course[subjectIndex].grade,
+                    nameList[number].time.slice(0)
+                );
+                if (available.length > 0) {
+                    let select = available[attemp < 1 ? 0 : Math.floor(Math.random() * available.length)]; //随机选出一门可用的课程
+                    rank = select.rank;
+                    for (let timeIndex in select.course.time) {
+                        nameList[number].time.push({
+                            start: select.course.time[timeIndex].start,
+                            weekday: select.course.time[timeIndex].weekday,
+                            duration: select.course.time[timeIndex].duration
+                        });//增加个人时间占用
+                    }
+                    nameList[number].select.push({
+                            name: select.course.name,
+                            rank: rank,
+                            subject: subject
+                        }
                     );//临时记录已选课程
-                successCount++;
-                log('[+] '+nameList[number].name+' 安排了：'+select.course.name);
-            }
-            else{
-                if(nameList[number].course[subjectIndex].grade<=lowControl[subject]){
                     successCount++;
-                    log('[-]警告 '+nameList[number].name+' 的：'+subject+' 低于分数区间');
+                    log('[+] ' + nameList[number].name + ' 安排了：' + select.course.name);
+                    //log('占用时间 开始时间：' + nameList[number].time[timeIndex].start + '持续时间： ' + nameList[number].time[timeIndex].duration + '星期：'+nameList[number].time[timeIndex].duration);
+                    for (let timeIndex in nameList[number].time) {
+                        //log('占用时间 开始时间：' + nameList[number].time[timeIndex].start + '持续时间： ' + nameList[number].time[timeIndex].duration + '星期：'+nameList[number].time[timeIndex].weekday);
+                    }
                 }
                 else {
-                    successCount = 0;
-                    log('[-] ' + nameList[number].name + ' 由于：' + subject + ' 选课失败');
-                    for (let k in nameList[number].select) {
-                        log(nameList[number].select[k].subject + ':' + nameList[number].select[k].grade);
+                    if (nameList[number].course[subjectIndex].grade <= lowControl[subject]) {
+                        successCount++;
+                        log('[-]警告 ' + nameList[number].name + ' 的：' + subject + ' 低于分数区间');
+                        break;
                     }
-                    nameList[number].time = [];
-                    nameList[number].select = [];
-                    continue;
+                    else {
+                        successCount = 0;
+                        log('[-] ' + nameList[number].name + ' 由于：' + subject + ' 选课失败');
+                        for (let k in nameList[number].select) {
+                            log(nameList[number].select[k].subject + ':' + nameList[number].select[k].grade);
+                        }
+                        nameList[number].time = [];
+                        nameList[number].select = [];
+                        continue;
+                    }
+
                 }
 
+                log('[+] ' + nameList[number].name + ' 了安排：' + successCount + ' 选课数量：' + nameList[number].course.length);
             }
-            log('[+] '+nameList[number].name+' 了安排：'+successCount+' 选课数量：'+nameList[number].course.length);
-            if(successCount >= nameList[number].course.length)
-            {
-                log('[+] '+nameList[number].name+' 选课成功');
-                //进入此处表示该学生排课成功
-                for (let i in nameList[number].select){
-                    for(let j in course[nameList[number].select[i].subject][nameList[number].select[i].rank])
-                    {
-                        if(course[nameList[number].select[i].subject][nameList[number].select[i].rank][j].name ===
-                                nameList[number].select[i].name)
-                        {
-                            course[nameList[number].select[i].subject][nameList[number].select[i].rank][j].count++; //增加该课程已选人数
-                            if(!course[nameList[number].select[i].subject][nameList[number].select[i].rank][j].list){
-                                course[nameList[number].select[i].subject][nameList[number].select[i].rank][j].list =[];
-                            }
-                            course[nameList[number].select[i].subject][nameList[number].select[i].rank][j].list.push({
-                                number:number,
-                                name:nameList[number].name
-                            });//在该课程已选名单中增加该学生信息
-                        }
-                    }
-                }
-                success = true;
-                break;
+
+            if(attemp>=20){
+                log('[-]'+nameList[number].name+'无法排课');
             }
         }
         if(success){
@@ -171,7 +171,156 @@ for (let number in nameList)
         }
     }
 }
+*///不完善算法
 
+for (let number in nameList)
+{
+    let tempTime=[];
+    let tempSubject={};
+    let currentSubject = '';
+    Counter++;
+    for (let attemp=0;attemp<600;attemp++)//尝试阈值
+    {
+        //判断是否选课成功
+        let subjectCounter = 0;
+        for(let i in nameList[number].course){
+            if(tempSubject.hasOwnProperty(nameList[number].course[i].subject))
+            {
+                subjectCounter++;
+            }
+        }
+        if(subjectCounter>=nameList[number].course.length)
+        {
+            //TODO：选课成功写入course和nameList
+            for(let i in tempSubject){
+                let rank = tempSubject[i].rank;
+                let subject = tempSubject[i].subject;
+                let courseName = tempSubject[i].name;
+                for(let j in course[subject][rank])
+                {
+                    if(course[subject][rank][j].name === courseName)
+                    {
+                        course[subject][rank][j].count++;
+                        if(!course[subject][rank][j].hasOwnProperty('list'))
+                        {
+                            course[subject][rank][j].list = [];
+                        }
+                        course[subject][rank][j].list.push({
+                            number:number
+                        });
+                    }
+
+                }
+                nameList[number].select.push({
+                    name:courseName
+                });
+            }
+            successCounter++;
+            break;//跳出尝试循环
+        }
+        //~判断是否选课成功
+
+        let toSelect = nameList[number].course.filter((item)=>{
+            if(item.subject in tempSubject)
+            {
+                return false;
+            }
+            else
+                return true;
+        });
+        toSelect.sort((a,b)=>{
+            return getCourse(a.subject,a.grade,tempTime).length - getCourse(b.subject,b.grade,tempTime).length;
+        });
+
+        currentSubject = toSelect[0].subject;
+        let available = getCourse(currentSubject,toSelect[0].grade,tempTime);
+        if(available.length>0){
+            //有可用课程
+            let select = available[attemp<6?0:Math.floor(Math.random() * available.length)];
+            tempSubject[currentSubject]={
+                rank:select.rank,
+                name:select.course.name,
+                subject:currentSubject
+            };
+            for(let index in select.course.time)
+            {
+                tempTime.push(select.course.time[index]);
+            }
+        }
+        else{
+            //本轮尝试失败
+            log('[-] 在'+nameList[number].name+' 安排：'+currentSubject+' 时矛盾');
+            log('安排情况如下');
+            for (let subject in tempSubject)
+            {
+                log(tempSubject[subject].name);
+            }
+            tempTime = [];
+            tempSubject = {};
+
+        }
+
+    }
+}
+
+//导出到Excel
+let exportWorkBook = {
+    SheetNames : [],
+    Sheets : {}
+};
+let sheetName = '';
+let data = [];
+//生成按照学号排序的表格
+sheetName = '花名册';
+data.push(['班级','姓名','已选课程']);
+for(let number in nameList){
+    let temp = [nameList[number].classID,nameList[number].name];
+    for(let i in  nameList[number].select)
+    {
+        temp.push(nameList[number].select[i].name);
+    }
+    data.push(temp.slice(0));
+}
+exportWorkBook.SheetNames.push(sheetName);
+exportWorkBook.Sheets[sheetName] = xlsx.utils.aoa_to_sheet(data);
+
+//生成按课程排列的表格
+for(let subject in course){
+    for(let rank in course[subject])
+    {
+        for(let courseIndex in course[subject][rank])
+        {
+            sheetName = course[subject][rank][courseIndex].name;
+            data = [['姓名','班级','成绩'],[course[subject][rank][courseIndex].name,'分数上界',course[subject][rank][courseIndex].highGrade]];
+            for(let studentIndex in course[subject][rank][courseIndex].list)
+            {
+                let number = course[subject][rank][courseIndex].list[studentIndex].number;
+                let temp = [];
+                temp.push([
+                    nameList[number].name,
+                    nameList[number].classID,
+                    nameList[number].course.filter((a)=>{
+                    if(a.subject == subject){
+                        return true;
+                    }
+                    })[0].grade
+                ]);
+                temp.sort((a,b)=>{
+                    return b[2] - a[2];
+                });
+                data = data.concat(temp);
+            }
+            data.push([course[subject][rank][courseIndex].name,'分数下界',course[subject][rank][courseIndex].lowGrade]);
+            data.push(['设定人数',course[subject][rank][courseIndex].amount,'实际人数',course[subject][rank][courseIndex].count]);
+            exportWorkBook.SheetNames.push(sheetName);
+            exportWorkBook.Sheets[sheetName] = xlsx.utils.aoa_to_sheet(data);
+        }
+    }
+}
+
+
+xlsx.writeFile(exportWorkBook,'export.xlsx');
+log('[+]共计排课 '+Counter+' 人，成功 '+successCounter+' 人');
 
 //utils functions
 //从中文名称转换到英文名称
@@ -191,7 +340,7 @@ function name2subject(name){
             return "chemistry";
         case '生物':
             return "biology";
-        case '思想品德':
+        case '政治':
             return "philosophy";
         case '地理':
             return "geography";
@@ -205,13 +354,15 @@ function name2subject(name){
 //检查时间可用性
 function checkTime(time,timeList){
     for(let i in timeList){
-        if(timeList[i].weekday === time.weekday){
-            if(timeList[i].start <= time.start+time.duration-1 &&time.start+time.duration-1 <= timeList[i].start+timeList[i].duration-1)
-                return false;
-            if(timeList[i].start <= time.start && time.start <= timeList[i].start+timeList[i].duration-1)
-                return false;
-            if(time.start <= timeList[i].start && time.start+time.duration-1 >=timeList[i].start+timeList[i].duration-1)
-                return false;
+        for(let j in time) {
+            if (timeList[i].weekday === time[j].weekday) {
+                if (timeList[i].start <= time[j].start + time[j].duration - 1 && time[j].start + time[j].duration - 1 <= timeList[i].start + timeList[i].duration - 1)
+                    return false;
+                if (timeList[i].start <= time[j].start && time[j].start <= timeList[i].start + timeList[i].duration - 1)
+                    return false;
+                if (time[j].start <= timeList[i].start && time[j].start + time[j].duration - 1 >= timeList[i].start + timeList[i].duration - 1)
+                    return false;
+            }
         }
     }
     return true;
@@ -229,7 +380,7 @@ class time extends Object
     }
 }
 
-//log(checkTime(new time(1,3,3),[]));
+console.log(checkTime(new time(6,4,1),[new time(6,5,1),new time(6,6,1),new time(6,1,1),new time(6,4,1)]));
 
 //获取可用科目列表
 function getCourse(subject, grade, timeList){
@@ -244,18 +395,18 @@ function getCourse(subject, grade, timeList){
                     if(!course[subject][rank][i].hasOwnProperty('count'))
                     {
                         course[subject][rank][i].count = 0;
-                        log('[-]人数未定义，初始化为:' + course[subject][rank][i].count);
+                        //log('[-]人数未定义，初始化为:' + course[subject][rank][i].count);
                     }
-                    if(course[subject][rank][i].count < course[subject][rank][i].amount*1.3)
+                    if(course[subject][rank][i].count <= course[subject][rank][i].amount*1.3)
                     {
                         result.push({rank:rank,course:course[subject][rank][i]});
                     }
                     else{
-                        log('[-]由于人数冲突失败','当前人数：'+course[subject][rank][i].count);
+                        log(course[subject][rank][i].name+'[-]由于人数冲突失败'+'当前人数：'+course[subject][rank][i].count);
                     }
                 }
                 else{
-                    log('[-]由于时间冲突失败');
+                    //log('[-]由于时间冲突失败');
                 }
             }
             break;
@@ -264,11 +415,13 @@ function getCourse(subject, grade, timeList){
            // log('[-]由于成绩不符合要求失败,科目'+subject+',高：'+course[subject][rank][0].highGrade+'，当前：'+grade+'，低：'+course[subject][rank][0].lowGrade)
         }
     }
-    //result.sort((a,b)=>{
-    //    return a.course.count - b.course.count;
-    //});
+    result.sort((a,b)=>{
+        return a.course.count - b.course.count;
+    });
     return result;
 }
+
+
 
 function log(msg) {
     fs.appendFileSync('log.txt',msg+'\n');
